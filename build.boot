4b015c488c6c2f2f233a6a577d0776b1662de258
anatoly <tolitius@gmail.com>
2017-07-28 14:00:49 -0400

    pluggable reporting with function args

--- build.boot
+++ build.boot
@@ -1,4 +1,4 @@
-(def +version+ "0.1.0-SNAPSHOT")
+(def +version+ "0.1.0")
 
 (set-env!
   :source-paths #{"src"}
--- src/calip/core.clj
+++ src/calip/core.clj
@@ -1,12 +1,23 @@
 (ns calip.core
   (:require [robert.hooke :as hooke]))
 
-(defn- calip [report fname f & args]
+(defn default-format [{:keys [fname took args]}]
+  (format "\"%s\" %s took: %,d nanos"
+          fname args took))
+
+(defn default-report [results]
+  (println (default-format results)))
+
+(defn- calip [{:keys [report]
+               :or {report default-report}} fname f & args]
   "wraps a function call with a timer: i.e. that times the function execution
    and reports the result"
   (let [start (System/nanoTime)
-        v (apply f args)]
-    (report (format "\"%s\" took: %,d nanos" fname (- (System/nanoTime) start)))
+        v (apply f args)
+        took (- (System/nanoTime) start)]
+    (report {:took took
+             :fname fname
+             :args args})
     v))
 
 (defn measure
@@ -19,12 +30,12 @@
 
   by default 'measure' will use 'println' to report times functions took"
   ([fs]
-   (measure fs {:report println}))
-  ([fs {:keys [report]}]
+   (measure fs {}))
+  ([fs opts]
    (doseq [f fs]
-     (hooke/add-hook f                              ;; target var
-                     (str f)                        ;; hooke key
-                     (partial calip report f)))))   ;; wrapper
+     (hooke/add-hook f                            ;; target var
+                     (str f)                      ;; hooke key
+                     (partial calip opts f)))))   ;; wrapper
 
 (defn uncalip [fs]
   "takes a set of functions (namespace vars) and removes times from them.
